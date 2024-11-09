ba1ba992837f29782de931698cba8a4aa4ba1fa9
Le Thuan Phat <163813842+PhatOi@users.noreply.github.com>
2024-11-09 21:24:45 +0700

    Create navbar.jsx

---
+++ src/pages/layout/navbar/navbar.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import "./css/navbar.css";
+import { NavLink } from "react-router-dom";
+import "boxicons/css/boxicons.min.css";
+
+function NavBar() {
+  return (
+    <nav className="navbar-nav">
+      <ul className="navbar-nav-ul">
+        <li className="navbar-nav-ul-li">
+          <NavLink to="/" className="navbar-nav-ul-li-link">
+            <i className="bx bx-message-rounded"></i>
+          </NavLink>
+        </li>
+        <li className="navbar-nav-ul-li">
+          <NavLink to="/" className="navbar-nav-ul-li-link">
+            <i className="bx bx-bell"></i>
+          </NavLink>
+        </li>
+        <li className="navbar-nav-ul-li">
+          <NavLink
+            to="/"
+            className="navbar-nav-ul-li-link navbar-nav-ul-li-link__admin"
+          >
+            <i className="bx bx-user-circle"></i>
+            <p>Lộc</p>
+          </NavLink>
+        </li>
+        <li className="navbar-nav-ul-li">
+          <NavLink to="/" className="navbar-nav-ul-li-link">
+            <i className="bx bx-cog"></i>
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavBar;
