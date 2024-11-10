a56a6a793845b7c1282b2fbf8ccf140bb1669beb
Le Thuan Phat <163813842+PhatOi@users.noreply.github.com>
2024-11-10 23:21:29 +0700

    Update navbar.jsx

--- src/pages/layout/navbar/navbar.jsx
+++ src/pages/layout/navbar/navbar.jsx
@@ -1,9 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+
 import "./css/navbar.css";
+import "./css/header.css";
+
 import { NavLink } from "react-router-dom";
 import "boxicons/css/boxicons.min.css";
 
+function Setting() {
+  return (
+    <div className="setting">
+      <div className="setting-block-navBar"></div>
+      <div className="setting-block paperNo">
+        <p className="line1">Số trang in</p>
+        <p>100</p>
+      </div>
+      <div className="setting-block logo AccountInfo">
+        <NavLink to="/" className="setting-block-link">
+          <i class="bx bx-user"></i>
+          <p>Thông tin tài khoản</p>
+        </NavLink>
+      </div>
+      <div className="setting-block logo logOut">
+        <NavLink to="/" className="setting-block-link">
+          <i class="bx bx-log-out"></i>
+          <p>Đăng xuất</p>
+        </NavLink>
+      </div>
+    </div>
+  );
+}
+
 function NavBar() {
+  const [showSetting, setShowSetting] = useState (false);
+
+  const handleShowSetting = () => {
+    setShowSetting(!showSetting);
+  }
+
   return (
     <nav className="navbar-nav">
       <ul className="navbar-nav-ul">
@@ -27,11 +60,12 @@ function NavBar() {
           </NavLink>
         </li>
         <li className="navbar-nav-ul-li">
-          <NavLink to="/" className="navbar-nav-ul-li-link">
-            <i className="bx bx-cog"></i>
-          </NavLink>
+          <div className="navbar-nav-ul-li-link">
+            <i className="bx bx-cog " onClick={handleShowSetting}></i>
+          </div>
         </li>
       </ul>
+      {showSetting && <Setting />}
     </nav>
   );
 }
