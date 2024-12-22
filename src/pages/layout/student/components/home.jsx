218219c61f15fc8894b64ad0166b473635b86678
Alcization <loc312004@gmail.com>
2024-12-22 20:56:39 +0700

    Remove unnecessary folder

--- src/pages/layout/student/components/home.jsx
+++ src/pages/layout/student/components/home.jsx
@@ -1,67 +1,3 @@
-// import React from "react";
-
-// import BuyPrintingPaper from "./img/BuyPrintingPaper.png";
-// import Library from "./img/Library.png";
-// import Printer from "./img/printer.png";
-// import PrintingHistory from "./img/PrintingHistory.png";
-
-// import { NavLink } from "react-router-dom";
-// import "./css/home/home.css";
-
-// function Home() {
-//   return (
-//     <div className="home container">
-//       <div className="Home row">
-//         <div className="home-block printingDocument col-md-3">
-//           <NavLink to={'/student/upload_file'} className="link printingDocument-nav-link">
-//             <img
-//               src={Printer}
-//               alt="Printer"
-//               className="img printingDocument-logo__img"
-//             />
-//             <p className="homeblocktitle">IN TÀI LIỆU</p>
-//           </NavLink>
-//         </div>
-
-//         <div className="home-block library col-md-3">
-//           <NavLink to={'/student/library'} className="link library-nav-link">
-//             <img
-//               src={Library}
-//               alt="Library"
-//               className="img library-logo__img"
-//             />
-//             <p className="homeblocktitle">THƯ VIỆN TÀI LIỆU</p>
-//           </NavLink>
-//         </div>
-
-//         <div className="home-block buyPrintingPaper col-md-3">
-//           <NavLink to={'/student/buy_printing_paper'} className="link buyPrintingPaper-nav-link">
-//             <img
-//               src={BuyPrintingPaper}
-//               alt="BuyPrintingPaper"
-//               className="img buyPrintingPaper-logo__img"
-//             />
-//             <p className="homeblocktitle">MUA TRANG IN</p>
-//           </NavLink>
-//         </div>
-
-//         <div className="home-block printingHistory col-md-3">
-//           <NavLink to={'/student/printingLog'} className="link printingHistory-nav-link">
-//             <img
-//               src={PrintingHistory}
-//               alt="PrintingHistory"
-//               className="img printingHistory-logo__img"
-//             />
-//             <p className="homeblocktitle">LỊCH SỬ IN ẤN</p>
-//           </NavLink>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Home;
-
 import React from "react";
 
 import BuyPrintingPaper from "./img/BuyPrintingPaper.png";
@@ -77,7 +13,7 @@ function Home() {
     <div className="home container">
       <div className="Home row">
 
-        <div className="home-block printingDocument col-md-4">
+        <div className="home-block printingDocument col-md-3">
           <NavLink to={'/student/upload_file'} className="link printingDocument-nav-link">
             <img
               src={Printer}
@@ -88,18 +24,18 @@ function Home() {
           </NavLink>
         </div>
 
-        <div className="home-block printingHistory col-md-4">
-          <NavLink to={'/student/printingLog'} className="link printingHistory-nav-link">
+        <div className="home-block library col-md-3">
+          <NavLink to={'/student/library'} className="link library-nav-link">
             <img
-              src={PrintingHistory}
-              alt="PrintingHistory"
-              className="img printingHistory-logo__img"
+              src={Library}
+              alt="Library"
+              className="img library-logo__img"
             />
-            <p className="homeblocktitle">LỊCH SỬ IN ẤN</p>
+            <p className="homeblocktitle">THƯ VIỆN TÀI LIỆU</p>
           </NavLink>
         </div>
 
-        <div className="home-block buyPrintingPaper col-md-4">
+        <div className="home-block buyPrintingPaper col-md-3">
           <NavLink to={'/student/buy_printing_paper'} className="link buyPrintingPaper-nav-link">
             <img
               src={BuyPrintingPaper}
@@ -107,6 +43,17 @@ function Home() {
               className="img buyPrintingPaper-logo__img"
             />
             <p className="homeblocktitle">MUA TRANG IN</p>
+          </NavLink>
+        </div>
+
+        <div className="home-block printingHistory col-md-3">
+          <NavLink to={'/student/printingLog'} className="link printingHistory-nav-link">
+            <img
+              src={PrintingHistory}
+              alt="PrintingHistory"
+              className="img printingHistory-logo__img"
+            />
+            <p className="homeblocktitle">LỊCH SỬ IN ẤN</p>
           </NavLink>
         </div>
 
